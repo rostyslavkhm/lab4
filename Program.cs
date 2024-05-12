1f0bb32aafd7310e6e92bf758f32f5ecfac978bf
rostyslavkhm <khmarskyirostick@gmail.com>
2024-05-12 02:06:54 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите путь к исходной директории:");
+        string sourceDir = Console.ReadLine();
+
+        if (!Directory.Exists(sourceDir))
+        {
+            Console.WriteLine("Ошибка: Указанной исходной директории не существует.");
+            return;
+        }
+
+        Console.WriteLine("Введите путь к целевой директории:");
+        string targetDir = Console.ReadLine();
+
+        if (!Directory.Exists(targetDir))
+        {
+            Console.WriteLine("Ошибка: Указанной целевой директории не существует.");
+            return;
+        }
+
+        Console.WriteLine("Введите количество файлов для копирования:");
+        if (!int.TryParse(Console.ReadLine(), out int filesToCopy))
+        {
+            Console.WriteLine("Неверный формат количества файлов.");
+            return;
+        }
+
+        Console.WriteLine("Введите шаблон для поиска файлов (например, *.exe):");
+        string searchPattern = Console.ReadLine();
+
+        try
+        {
+            string[] files = Directory.GetFiles(sourceDir, searchPattern);
+            int filesCopied = 0;
+
+            foreach (string file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string destFile = Path.Combine(targetDir, fileInfo.Name);
+
+                if ((fileInfo.Attributes & FileAttributes.Hidden) != 0)
+                {
+                    Console.WriteLine($"Файл {fileInfo.Name} является скрытым.");
+                    continue;
+                }
+                if ((fileInfo.Attributes & FileAttributes.ReadOnly) != 0)
+                {
+                    Console.WriteLine($"Файл {fileInfo.Name} доступен только для чтения.");
+                    continue;
+                }
+                if ((fileInfo.Attributes & FileAttributes.Archive) == 0)
+                {
+                    Console.WriteLine($"Файл {fileInfo.Name} не имеет атрибут архивации.");
+                    continue;
+                }
+
+                File.Copy(file, destFile, true);
+                Console.WriteLine($"Файл {fileInfo.Name} скопирован в {targetDir}");
+                filesCopied++;
+
+                if (filesCopied >= filesToCopy)
+                    break;
+            }
+
+            Console.WriteLine($"Копирование завершено. Скопировано {filesCopied} файлов из {filesToCopy} запланированных.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
